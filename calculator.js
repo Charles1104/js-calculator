2ee2fd977f4010767a3a349a4a95c4b5296abaff
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-03-14 17:47:51 -1000

    all tests passed

--- calculator.js
+++ calculator.js
@@ -6,6 +6,13 @@
  * @return {object} `calculator` object that can be used
  */
 
+ var calculatorModule = (function () {
+
+  //private variables
+  var memory = 0;
+  var total = 0;
+  var calculator = {};
+
 
   /**
    * sets the `total` to the number passed in
@@ -13,11 +20,23 @@
    * @return { Number }    current total
    */
 
+  calculator.load = function(number){
+    if (isNumber(number)){
+      total = number;
+      return total;
+    }
+
+  };
+
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
+
+   calculator.getTotal = function(){
+    return total;
+   };
 
 
   /**
@@ -25,11 +44,24 @@
    * @param { Number } x
    */
 
+   calculator.add = function (number){
+    if (isNumber(number)){
+      total += number;
+      }
+   };
+
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
+
+   calculator.subtract = function (number){
+    if (isNumber(number)){
+      total -= number;
+    }
+   };
+
 
 
   /**
@@ -37,29 +69,62 @@
    * @param  { Number } x
    */
 
+    calculator.multiply = function (number){
+    if (isNumber(number)){
+      total *= number;
+    }
+   };
+
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
-
+    calculator.divide = function (number){
+    if (isNumber(number)){
+      total /= number;
+    }
+   };
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
 
-
+   calculator.recallMemory = function(){
+    return memory;
+   };
   /**
    * Stores the value of `total` to `memory`
    */
-
+   calculator.saveMemory = function (){
+    memory = total;
+   };
 
   /**
    * Clear the value stored at `memory`
    */
 
+   calculator.clearMemory = function(){
+    memory = 0;
+   };
+
+
   /**
    * Validation
    */
 
+  function isNumber(testValue) {
+    if (typeof testValue === "number") {
+      return true;
+    }
+    else {
+     throw new Error('Wrong data type you dummy');
+    }
+  }
+
+  return calculator;
+ });
+
+var newcal = calculatorModule();
+console.log(newcal);
